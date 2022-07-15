020c08913980c91a9a21acd0533d59b1eab83471
KirillShk <109240991+KirillShk@users.noreply.github.com>
2022-07-15 14:57:19 +0300

    Update Program.cs

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -17,7 +17,7 @@ namespace Task_5
             {
                 for (int j = 0; j < n; j++)
                 {
-                    array[i, j] = ((i == j) || (i + j == n - 1) || (i + j == n - 3) || (i + j == n + 1)) ? 1: 0;
+                    array[i, j] = ((i == j) || (i + j == n - 1) || (i + j == n /2) || (i + j == n +(n/2 - 1))) ? 1: 0;;
                     Console.Write("{0} ", array[i, j]);
                 }
             Console.WriteLine();
